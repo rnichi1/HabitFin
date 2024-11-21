7f83718ab69844185a6a071aa8e3702c239526e5
rnichi1 <71671446+rnichi1@users.noreply.github.com>
2024-11-21 07:48:14 +0100

    Initial Commit

---
+++ HabitFinTests/HabitFinTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFinTests.swift
+//  HabitFinTests
+//
+//  Created by Remus on 21.11.2024.
+//
+
+import Testing
+@testable import HabitFin
+
+struct HabitFinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
